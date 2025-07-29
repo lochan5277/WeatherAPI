98ed17ac09d86d3fa036e410ec70f9fa1866dbed
lochan87 <1DS22IS073@dsce.edu.in>
2025-07-29 17:23:43 +0530

    commit1

---
+++ Shared/Models/UserDto.cs
@@ -0,0 +1,10 @@
+namespace WeatherApp_API.Shared.Models;
+
+public class UserDto
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? FavoriteCity { get; set; }
+    public string? FavoriteWeather { get; set; }
+}
+
---
+++ Shared/Models/WeatherInfo.cs
@@ -0,0 +1,8 @@
+namespace WeatherApp_API.Shared.Models;
+
+public class WeatherInfo {
+    public string City { get; set; }
+    public string Description { get; set; }
+    public float Temperature { get; set; }
+    public DateTime Date { get; set; }
+}
